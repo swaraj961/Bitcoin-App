a676e5c7826451bf93acc1ec8e5fa9522c351d44
Swaraj961 <swarajroutray961@gmail.com>
2020-04-07 17:47:26 +0530

    ETH and LTC Cards ADDED

--- lib/Price_screen.dart
+++ lib/Price_screen.dart
@@ -101,21 +101,21 @@ setState(() {
         centerTitle: true,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
+            child: Card( //card1
               color: Color(0xFFFFA000),
-              elevation: 5.0,
+              elevation: 25.0,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
               ),
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1BTC = $bitcoinvalue $selectedcurreency',
+                  '1 BTC = $bitcoinvalue $selectedcurreency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -125,6 +125,56 @@ setState(() {
                 ),
               ),
             ),
+          ),
+        
+          Padding(
+            padding: EdgeInsets.fromLTRB(18, 18, 18, 0),
+                      child: Card( //card2
+            
+                color: Color(0xFFFFA000),
+                elevation: 25.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                  child: Text(
+                    '1 ETH = $bitcoinvalue $selectedcurreency',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600
+                    ),
+                  ),
+                ),
+              ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18, 18, 18, 0),
+                      child: Card( //card3
+                color: Color(0xFFFFA000),
+                elevation: 25.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                  child: Text(
+                    '1 LTC = $bitcoinvalue $selectedcurreency',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600
+                    ),
+                  ),
+                ),
+              ),
+          ),
+          
+          SizedBox(
+            height:150,
           ),
           Container(
               height: 150.0,
